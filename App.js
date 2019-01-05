dbd06745f22372ca5fe68d7e08b378b9003c7151
50knotsrotate <42186149+50knotsrotate@users.noreply.github.com>
2019-01-05 10:58:34 -0500

    Create App.js

---
+++ App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Input from './Input'
+
+class App extends Component {
+  
+
+  render() {
+    return (
+      <div className="App">
+      <Input />
+      
+     
+        
+      </div>
+    );
+  }
+}
+
+export default App;
